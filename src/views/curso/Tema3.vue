c5528a899b576f19a948bce28145454d253114b5
AndresDiaz <devcontenidosinteractivos@gmail.com>
2025-03-11 09:32:45 -0500

    tema 3 ok
    Se finaliza tema 3 ok

--- src/views/curso/Tema3.vue
+++ src/views/curso/Tema3.vue
@@ -1,12 +1,135 @@
 <template lang="pug">
 .curso-main-container.pb-3
-  BannerInterno(:subTitulo="'3. Título tema'")
+  BannerInterno(:subTitulo="'3. Conceptos de poder organizacional, política y mecanismos de control'")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5.overflow-hidden
 
-    #t_3_1.titulo-segundo(data-aos="flip-up")
-      h2 #[span 3.1] Subtítulo tema
+    .row.align-items-center.bg-h85-3
+      .col-lg-3(data-aos="fade-left")
+        img(src="@/assets/curso/tema3/1.svg").mx-auto
+      .col-lg-9.p-5
+        p.mb-0 El poder organizacional, la política interna y los mecanismos de control influyen en la toma de decisiones, la distribución de recursos y la gestión del comportamiento dentro de una organización.
+    .row.align-items-center.bg-h85-4
+      .col-lg-3(data-aos="fade-left").order-1.order-lg-2
+        img(src="@/assets/curso/tema3/2.svg").mx-auto
+      .col-lg-9.order-2.order-lg-1.p-5
+        p.mb-0 El poder organizacional se refiere a la capacidad de influir en los demás para alcanzar objetivos estratégicos, mientras que la política organizacional abarca las dinámicas, negociaciones y alianzas que surgen dentro de la empresa.
 
+    p.mb-4 Por su parte, los mecanismos de control permiten garantizar que las acciones y procesos se alineen con los objetivos organizacionales, asegurando eficiencia y cumplimiento. A continuación, se desglosa de manera profunda cada uno de estos elementos.
+
+    #t_3_1.titulo-segundo(data-aos="flip-up")
+      h2 #[span 3.1] Poder organizacional
     
+    .row.mb-5.align-items-center
+      .col-lg-3.mb-3.mb-lg-0
+        figure
+          img(src="@/assets/curso/tema3/3.svg", data-aos="zoom-in")
+      .col-lg-9
+        p(data-aos="fade-left") El poder organizacional es la capacidad que tienen los individuos o grupos dentro de una organización para influir en las decisiones, comportamientos y recursos con el fin de alcanzar determinados objetivos. 
+        p En el ámbito empresarial, el poder se manifiesta a través de diversas formas, como la autoridad formal, la pericia, el control de la información y la capacidad de influir en otros a través de la negociación, la legalidad o la persuasión. 
+        p.mb-0 Existen distintas fuentes de poder en una organización: 
+    
+    .row.justify-content-center.align-items-stretch.mb-3
+      .col-lg-4.mb-3.mb-lg-0
+        .bg-color-3.p-4(data-aos="fade-left").h-100
+          .row.align-items-center
+            .col-lg-auto
+              img(src="@/assets/curso/tema3/4.svg", style="max-width: 90px").mx-auto
+            .col-lg
+              h5.mb-2 El poder legítimo
+              p.mb-0 Este poder emana de la posición jerárquica.
+
+      .col-lg-4.mb-3.mb-lg-0
+        .bg-color-3.p-4(data-aos="fade-left").h-100
+          .row.align-items-center
+            .col-lg-auto
+              img(src="@/assets/curso/tema3/5.svg", style="max-width: 90px").mx-auto
+            .col-lg
+              h5.mb-2 El poder coercitivo
+              p.mb-0 Está basado en la capacidad de imponer sanciones.
+      .col-lg-4.mb-3.mb-lg-0
+        .bg-color-3.p-4(data-aos="fade-left").h-100
+          .row.align-items-center
+            .col-lg-auto
+              img(src="@/assets/curso/tema3/6.svg", style="max-width: 90px").mx-auto
+            .col-lg
+              h5.mb-2 El poder de recompensa
+              p.mb-0 Este poder depende de la capacidad de otorgar incentivos.
+
+    .row.justify-content-center.align-items-stretch.mb-5
+      .col-lg-4.mb-3.mb-lg-0
+        .bg-color-3.p-4(data-aos="fade-left").h-100
+          .row.align-items-center
+            .col-lg-auto
+              img(src="@/assets/curso/tema3/7.svg", style="max-width: 90px").mx-auto
+            .col-lg
+              h5.mb-2 El poder experto
+              p.mb-0 Se deriva del conocimiento o habilidades especiales.
+
+      .col-lg-4.mb-3.mb-lg-0
+        .bg-color-3.p-4(data-aos="fade-left").h-100
+          .row.align-items-center
+            .col-lg-auto
+              img(src="@/assets/curso/tema3/8.svg", style="max-width: 90px").mx-auto
+            .col-lg
+              h5.mb-2 El poder de referencia
+              p.mb-0 Está basado en el carisma o influencia personal.
+    
+    .bg-full-width.bg-color-10.mb-5
+      .px-4.px-md-5.py-4
+        p.mb-0.text-white La correcta gestión del poder organizacional es vital para mantener un ambiente laboral equilibrado y evitar conflictos que puedan afectar la productividad y el clima organizacional.
+    
+    #t_3_2.titulo-segundo(data-aos="flip-up")
+      h2 #[span 3.2] Política organizacional
+    
+    p.mb-4 La política organizacional se refiere a las estrategias, comportamientos y acciones que los individuos y grupos dentro de una empresa utilizan para influir en la distribución de recursos, la toma de decisiones y el desarrollo de la organización en general.
+
+    .row.mb-5
+      .col-lg-4.mb-3.mb-lg-0
+        figure
+          img(src="@/assets/curso/tema3/9.png", data-aos="zoom-in")
+      .col-lg-8
+        p(data-aos="fade-left") Es un fenómeno inherente a cualquier estructura organizativa y puede manifestarse de manera formal, a través de reglamentos y normas establecidas, o informalmente, mediante alianzas, redes de influencia y juegos de poder. Aunque la política organizacional puede ser vista como una práctica negativa cuando se asocia con manipulaciones o favoritismos, también puede ser utilizada de manera positiva para promover el cambio, impulsar el crecimiento y defender los intereses de la organización. 
+    
+        .bg-color-2.p-4(data-aos="fade-left")
+          .row.align-items-start
+            .col-lg-auto
+              img(src="@/assets/curso/tema3/10.svg", style="max-width: 90px").mx-auto
+            .col-lg
+              p.mb-0 Una gestión adecuada de la política organizacional permite que la empresa se adapte a entornos cambiantes y logre sus objetivos de manera más efectiva, favoreciendo la comunicación, la colaboración y el liderazgo efectivo.
+    
+    #t_3_3.titulo-segundo(data-aos="flip-up")
+      h2 #[span 3.3] Mecanismos de control
+
+    p Los mecanismos de control en una organización son las herramientas, procesos y sistemas implementados para garantizar que las actividades y operaciones se realicen conforme a los objetivos, políticas y regulaciones establecidas. 
+    p.mb-4 El control representa un mecanismo necesario para monitorear el desempeño, reducir riesgos y fomentar la eficiencia en el uso de los recursos. Entre los mecanismos de control más importantes se encuentran:
+
+    .row.align-items-start.mb-5
+      .col-lg-4.order-2
+        figure
+          img(src="@/assets/curso/tema3/11.png", alt="").mb-4.mb-lg-0
+      .col-lg-8.mb-3.mb-lg-0.order-1
+    
+        AcordionA(tipo="b")
+          .div(titulo="Control burocrático")
+            p.mb-0 Se basa en normas, políticas y procedimientos formales para regular el comportamiento dentro de la organización. Su aplicación es común en entornos altamente estructurados, donde se busca minimizar la ambigüedad y garantizar la uniformidad en los procesos. Su importancia radica en la previsibilidad y consistencia de las operaciones.
+    
+          .div(titulo="Control de mercado")
+            p.mb-0 Utiliza indicadores financieros, precios y competencia para evaluar el desempeño organizacional. Se aplica en empresas con fuerte orientación al mercado, permitiendo ajustar estrategias en función del comportamiento de los consumidores y la competencia. Su importancia radica en la capacidad de adaptación y la mejora de la rentabilidad.
+    
+          .div(titulo="Control cultural")
+            p Se enfoca en la creación de una cultura organizacional sólida con valores y creencias compartidas que guíen la conducta de los empleados. Se aplica mediante la promoción de valores corporativos, liderazgo inspirador y programas de desarrollo organizacional. Su importancia radica en fomentar el compromiso y la cohesión entre los empleados, impulsando la productividad.
+          .div(titulo="Control directivo")
+            p Implica la supervisión directa de los gerentes y líderes sobre sus equipos de trabajo para asegurar el cumplimiento de objetivos. Se aplica a través de reuniones de seguimiento, evaluaciones de desempeño y revisión de indicadores claves. Su importancia radica en la orientación y alineación de esfuerzos en la organización.
+          .div(titulo="Control de retroalimentación")
+            p Evalúa los resultados obtenidos y los compara con los objetivos planeados para corregir desviaciones. Se aplica mediante auditorías, informes de desempeño y revisión de KPIs. Su importancia radica en la mejora continua y la capacidad de respuesta ante problemas operativos.
+    
+    .bg-full-width.bg-color-11
+      .px-4.px-md-5.py-4
+        .row.align-items-center
+          .col-lg-auto
+            img(src="@/assets/curso/tema3/12.svg", style="max-width: 90px").mx-auto
+          .col-lg
+            p.mb-0 Para garantizar la eficiencia y sostenibilidad de la organización, es necesario la aplicación de los anteriores elementos. Un sistema de control efectivo minimiza errores y fraudes, pero además fomenta la innovación y mejora la toma de decisiones al proporcionar información relevante sobre el desempeño organizacional.
 
     .bg-full-width.border-top.color-primario
       .p-4.p-md-5
@@ -17,15 +140,16 @@
 
             p.d-flex.my-4
               img.me-3(src='@/assets/componentes/link.svg' :style="{'max-width':'16px'}")
-              a(href="https://elibro.net/es/lc/tecnologicadeloriente/titulos/274611" target="_blank" rel="noopener noreferrer") De Castro Emilio Cerdá, F. J. A. L. M. (s/f). Las energías renovables en el ámbito internacional. Revistasice.com. 
-
+              a(href="https://www.redalyc.org/articulo.oa?id=322327241005" target="_blank" rel="noopener noreferrer") Arango Restrepo, M. R. (2007). Control organizacional: ¿sistema o agregado de elementos? Ministro AD, 11, 1-18. 
             p.d-flex.my-4
-              img.me-3(src='@/assets/template/book.svg' :style="{'max-width':'16px'}")
-              span De Janeiro, R. (1992). La declaración del Río sobre el Medio Ambiente y el Desarrollo. Río de Janeiro República Federativa del Brasil., aprobada en Estocolmo el, 16.
-
+              img.me-3(src='@/assets/componentes/link.svg' :style="{'max-width':'16px'}")
+              a(href="https://gestionyestrategia.azc.uam.mx/index.php/rge/article/view/102 " target="_blank" rel="noopener noreferrer") Wilhelm Vázquez García, Á. (2011). Teorías del cambio organizacional: una síntesis. Gestión y Estrategia, 40, 1-15.
             p.d-flex.my-4
               img.me-3(src='@/assets/template/icono-yt.svg' :style="{'max-width':'16px'}")
-              a(href="https://youtu.be/QKLjwCMyv3M?si=uAWhO9tnyghljaJg" target="_blank" rel="noopener noreferrer") Innovación y sostenibilidad.
+              a(href="https://youtu.be/K4MyStognjA?si=sp3Fjl7rld32Ri8e" target="_blank" rel="noopener noreferrer") Ricardo Peri (2014, 10 de octubre) Cambio Estratégico Organizacional. [Video]. Youtube.
+            p.d-flex.my-4
+              img.me-3(src='@/assets/template/icono-yt.svg' :style="{'max-width':'16px'}")
+              a(href="https://youtu.be/AokBaocTH9Y?si=7eoBZ7CN3bV70wEK" target="_blank" rel="noopener noreferrer") Estudiando Empresa y Estrategia (2021, 10 de octubre) El control en la empresa. [Video] Youtube. 
 
           .col-12.col-md-6.col-lg-3.offset-lg-1
             figure
